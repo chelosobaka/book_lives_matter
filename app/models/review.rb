831388b05c809e45240ba022b93af35ba78d9675
chelosobaka <Chelosobaka93@gmail.com>
2022-03-14 02:30:22 +0300

    fake_reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,13 @@ class Review < ApplicationRecord
   validates :rating, presence: true, inclusion: {in: RATING}
 
   scope :lately, lambda{where "updated_at >= ?", 2.days.ago}
+
+  def self.fake_review
+    User.all.ids.each do |bl_user_id|
+      Book.all.ids.sample(10).each do |bl_book_id|
+        Review.create(book_id: bl_book_id, user_id: bl_user_id,
+           rating: Review::RATING.sample, body: Faker::Quote.matz)
+      end
+    end
+  end
 end
